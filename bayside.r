33f574d7f8bd9499353da47f537f90ec7ac2d78c
sedie <sedie@uchicago.edu>
2017-03-23 12:37:29 -0500

    tidy master bayside exec script

--- bayside.r
+++ bayside.r
@@ -1,16 +1,15 @@
 # wrap for running bayside model
-# setwd("~/GoogleDrive/NumBivSpp/bayside")
 args <- commandArgs(trailingOnly=TRUE)
-OFFSET <- args[1]
-ftime <- args[2]
+OFFSET <- args[1]  # TRUE = include publications in model, FALSE = do not include publications
+ftime <- args[2]  # number of years into the future to forecast species richness (integer; 0 -> INF)
 
 # run data
-system( paste0("R CMD BATCH --vanilla '--args ", OFFSET, "' code/data.r") )
+system(paste0("R CMD BATCH --vanilla '--args ", OFFSET, "' code/data.r"))
 # run model
 system("R CMD BATCH --vanilla code/model.r")
 # run post
 system("R CMD BATCH --vanilla code/post.r")
 # run forecast
-system(paste0("R CMD BATCH --vanilla '--args ", ftime, "' code/forecast.r ") )
+system(paste0("R CMD BATCH --vanilla '--args ", ftime, "' code/forecast.r "))
 # run plot
-system(paste0("R CMD BATCH --vanilla '--args ", OFFSET, "' code/plot.r") )
+system(paste0("R CMD BATCH --vanilla '--args ", OFFSET, "' code/plot.r"))
